9b4a01f1958469bdf8b44c86700f9d32b363b6e3
unknown <kenneorla1@gmail.com>
2026-08-03 13:17:05 +0100

    i added onned icon to the chats

--- src/components/sidebar/ChatListItem.jsx
+++ src/components/sidebar/ChatListItem.jsx
@@ -36,6 +36,12 @@ export default function ChatListItem({ chat, isActive, onClick }) {
                 <path d="M12 4L9.91 6.09 12 8.18V4zM18 12c0-3.31-2.69-6-6-6v1.79l5.57 5.57c.14-.3.26-.61.35-.94l.08-.42zm-8 6.59V16H7v-4c0-.76.15-1.49.43-2.16L3.28 7.87 2 9.16l2.1 2.1C4.04 11.68 4 11.84 4 12v2H2v2h8v2.59l4.09-4.09L16 16.59 10 22.59z" />
               </svg>
             )}
+            
+             {chat.pinned && (
+              <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor" className="chat-list-item__pinned" style={{ transform: 'rotate(45deg)' }}>
+                <path d="M16 12V4h1V2H7v2h1v8l-2 2v2h5.2v6h1.6v-6H18v-2l-2-2z" />
+              </svg>
+            )}
             {chat.unreadCount > 0 && <Badge count={chat.unreadCount} />}
           </div>
         </div>
