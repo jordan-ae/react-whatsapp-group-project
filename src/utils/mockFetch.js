9cf976c8eca73c91db19426e335e1726b82a2174
Palvett Blaise <blaisepalvett@gmail.com>
2026-08-04 13:37:08 +0100

    removed the delay and added the flex-shrink to the profile avatar

--- src/utils/mockFetch.js
+++ src/utils/mockFetch.js
@@ -1,4 +1,4 @@
-const delay = (ms) => new Promise((res) => setTimeout(res, 3000));
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
 
 export const mockFetch = async (url, options = {}) => {
   await delay(200 + Math.random() * 300);
