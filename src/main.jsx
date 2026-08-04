76e2150c7c687e0b89954211a48a903f399a9fa5
Palvett Blaise <blaisepalvett@gmail.com>
2026-08-04 14:30:07 +0100

    updated the files to match the feat/profile-1.2 branch because they basically do the same thing

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
 import App from './App.jsx'
+import './styles/skeleton.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
