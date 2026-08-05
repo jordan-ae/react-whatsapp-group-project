026ae25dd33ddc356cd44ec2c15aaeb6b7cad5da
Valery <Nyoh Jonathan Smith>
2026-08-05 09:55:31 +0100

    fix: classname naming error

--- src/components/status/StatusViewer.jsx
+++ src/components/status/StatusViewer.jsx
@@ -91,9 +91,7 @@ export default function StatusViewer({ status, onClose }) {
                               fallbackLabel="This status image couldn't be loaded"
                             />
 
-                   <h3 className="status-viewer_image-text">{currentItem.text}</h3>
-
-                  <h3 className="status-viewer__timage-text">{currentItem.text}</h3>
+                  <h3 className="status-viewer__image-text">{currentItem.text}</h3>
                    <p>{currentItem.caption}</p>
                 </div>
             )}
