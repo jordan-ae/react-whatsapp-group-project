3a8572b5a8d7fb8bc26474caf8d0d15a53af7084
Losha Stanice <kieramaxswell@gmail.com>
2026-08-07 13:11:31 +0100

    Trying to push

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -8,7 +8,7 @@ import MessageInput from "../components/chat/MessageInput";
 import { formatDateLabel } from "../utils/formatDate";
 import "./ChatPage.css";
 
-export default functions ChatPage() {
+export default function ChatPage() {
   const { selectedChatId } = useApp();
   const { chats } = useChats();
   const { messages, loading, refetch } = useChatMessages(selectedChatId);
